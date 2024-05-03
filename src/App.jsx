b18d715f52970d13a2d3304a8ebcf822ba5e58df
Natalie <natchc@gmail.com>
2024-05-03 18:50:33 +0100

    initialise tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    
-    </>
-  )
+    <div>
+      <h1 className="text-3xl font-bold text-red-700">Hello world!</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
